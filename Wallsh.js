7169efc1c83536201a2cb65e6495391bcf50315b
Aditya Gupta <adityaakadynamo@users.noreply.github.com>
2016-07-27 18:11:04 +0530

    Create Wallsh.js

---
+++ Wallsh.js
@@ -0,0 +1,18 @@
+[
+    {
+    "name" : "Blak Ui", 
+    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/blak_new.png"
+    },
+    {
+    "name" : "Blak Ui", 
+    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/blak_new.png"
+    },
+    {
+    "name" : "Blak Ui", 
+    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/blak_new.png"
+    },
+    {
+    "name" : "Blak Ui", 
+    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/blak_new.png"
+    }
+]
