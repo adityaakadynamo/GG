58260df669ea856ce1951f3051b82cd5cf15e14d
Aditya Gupta <adityaakadynamo@users.noreply.github.com>
2016-07-27 18:42:21 +0530

    Update Wallsh.js

--- Wallsh.js
+++ Wallsh.js
@@ -4,15 +4,39 @@
     "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/blak_new.png"
     },
     {
-    "name" : "Blak Ui", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/blak_new.png"
+    "name" : "Rise Ui", 
+    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/rise_ui_small.png"
     },
     {
-    "name" : "Blak Ui", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/blak_new.png"
+    "name" : "Card Ui", 
+    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Cherry_small.png"
     },
     {
-    "name" : "Blak Ui", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/blak_new.png"
+    "name" : "Dazz", 
+    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Dazz_small.png"
+    },
+    {
+    "name" : "Radiant", 
+    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Radiant_small.png"
+    },
+    {
+    "name" : "Radiant1", 
+    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Radiant1_small.png"
+    },
+    {
+    "name" : "Grace", 
+    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Graze_small.png"
+    },
+    {
+    "name" : "Flip", 
+    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Flip_small.png"
+    },
+    {
+    "name" : "Md1", 
+    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Md1_small.png"
+    },
+    {
+    "name" : "Md", 
+    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Md_small.png"
     }
 ]
