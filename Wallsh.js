d56f6e58d6fc14668af633b48ab12e16dd1156e6
Aditya Gupta <adityaakadynamo@users.noreply.github.com>
2016-08-15 13:21:14 +0530

    Update Wallsh.js

--- Wallsh.js
+++ Wallsh.js
@@ -1,219 +1,220 @@
 [
-    {
-     "category_name" : "Nature", 
-     "walls" : [
-         {
-    "name" : "Blak Ui", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Blak_wall.png"
-    },
-    {
-    "name" : "Rise Ui", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/rise_ui.png"
-    },
-    {
-    "name" : "Card Ui", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Cherry.png"
-    },
-    {
-    "name" : "Dazz", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Dazz.png"
-    },
-    {
-    "name" : "Radiant", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Radiant.png"
-    },
-    {
-    "name" : "Radiant1", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Radiant1.png"
-    },
-    {
-    "name" : "Grace", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Grace.png"
-    },
-    {
-    "name" : "Flip", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Flip.png"
-    },
-    {
-    "name" : "Md1", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Md1.png"
-    },
-    {
-    "name" : "Md", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Md.png"
-    },
-    {
-    "name" : "Paper Dark", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Paper_dark.png"
-    },
-    {
-    "name" : "Paper", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Paper.jpg"
-    },
-    {
-    "name" : "Pearl", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Pearl.png"
-    },
-    {
-    "name" : "Vivid", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Vivid.png"
-    },
-    {
-    "name" : "Weyo", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Weyo.png"
-    },
-    {
-    "name" : "Blade", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Blade.png"
-    },
-    {
-    "name" : "Blush", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Blush.png"
-    },
-    {
-    "name" : "Blush1", 
-    "link" : "http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Blush1.png"
-    },
-    {
-    "name" : "Bold", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Bold.jpg"
-    },
-    {
-    "name" : "Inspire", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Inspire.png"
-    },
-    {
-    "name" : "Prime", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Prime.png"
-    },
-    {
-    "name" : "Random", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Random.png"
-    },
-    {
-    "name" : "Ace", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Ace.png"
-    },
-    {
-    "name" : "Arena", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Arena.png"
-    },
-    {
-    "name" : "Arena1", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Arena1.png"
-    },
-    {
-    "name" : "Ascend", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Ascend.png"
-    },
-    {
-    "name" : "Auric1", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Auric1.png"
-    },
-    {
-    "name" : "Blade", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Blade.png"
-    },
-    {
-    "name" : "Blak", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Blak.png"
-    },	
-    {
-    "name" : "Blush", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Blush.png"
-    },
-    {
-    "name" : "Bora", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Bora.png"
-    },
-    {
-    "name" : "Britzer", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Britzer.png"
-    },
-    {
-    "name" : "Britzer1", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Britzer1.jpg"
-    },
-    { 
-    "name" : "CardUi", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/CardUi.png"
-    },
-    {
-    "name" : "Cherry", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Cherry.png"
-    },
-    {
-    "name" : "Dare", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dare.png"
-    },
-    {
-    "name" : "Dare1", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dare1.png"
-    },
-    {
-    "name" : "Dazz", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dazz.png"
-    },
-    {
-    "name" : "Eco", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Eco.png"
-    },
-    {
-    "name" : "Edge", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge.png"
-    },
-    {
-    "name" : "Edge1", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge1.png"
-    },
-    {
-    "name" : "Edge2", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge2.png"
-    },
-    {
-    "name" : "Eminence", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Eminence.png"
-    }
-         ]
-    },
-    {
-     "category_name" : "Baloon", 
-     "walls" : [
-    {
-    "name" : "Cherry", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Cherry.png"
-    },
-    {
-    "name" : "Dare", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dare.png"
-    },
-    {
-    "name" : "Dare1", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dare1.png"
-    },
-    {
-    "name" : "Dazz", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dazz.png"
-    },
-    {
-    "name" : "Eco", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Eco.png"
-    },
-    {
-    "name" : "Edge", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge.png"
-    },
-    {
-    "name" : "Edge1", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge1.png"
-    },
-    {
-    "name" : "Edge2", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge2.png"
-    },
-    {
-    "name" : "Eminence", 
-    "link" : "https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Eminence.png"
-    }
-         ]
+  {
+    "category_name":"Nature",
+    "walls":[
+      {
+        "name":"Blak Ui",
+        "link":"http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Blak_wall.png"
+      },
+      {
+        "name":"Rise Ui",
+        "link":"http://www.bigdx.com/userwalls/ideadesigns/WallsHd/rise_ui.png"
+      },
+      {
+        "name":"Card Ui",
+        "link":"http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Cherry.png"
+      },
+      {
+        "name":"Dazz",
+        "link":"http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Dazz.png"
+      },
+      {
+        "name":"Radiant",
+        "link":"http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Radiant.png"
+      },
+      {
+        "name":"Radiant1",
+        "link":"http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Radiant1.png"
+      },
+      {
+        "name":"Grace",
+        "link":"http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Grace.png"
+      },
+      {
+        "name":"Flip",
+        "link":"http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Flip.png"
+      },
+      {
+        "name":"Md1",
+        "link":"http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Md1.png"
+      },
+      {
+        "name":"Md",
+        "link":"http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Md.png"
+      },
+      {
+        "name":"Paper Dark",
+        "link":"http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Paper_dark.png"
+      },
+      {
+        "name":"Paper",
+        "link":"http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Paper.jpg"
+      },
+      {
+        "name":"Pearl",
+        "link":"http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Pearl.png"
+      },
+      {
+        "name":"Vivid",
+        "link":"http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Vivid.png"
+      },
+      {
+        "name":"Weyo",
+        "link":"http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Weyo.png"
+      },
+      {
+        "name":"Blade",
+        "link":"http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Blade.png"
+      },
+      {
+        "name":"Blush",
+        "link":"http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Blush.png"
+      },
+      {
+        "name":"Blush1",
+        "link":"http://www.bigdx.com/userwalls/ideadesigns/WallsHd/Blush1.png"
+      },
+      {
+        "name":"Bold",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Bold.jpg"
+      },
+      {
+        "name":"Inspire",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Inspire.png"
+      },
+      {
+        "name":"Prime",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Prime.png"
+      },
+      {
+        "name":"Random",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Random.png"
+      },
+      {
+        "name":"Ace",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Ace.png"
+      },
+      {
+        "name":"Arena",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Arena.png"
+      },
+      {
+        "name":"Arena1",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Arena1.png"
+      },
+      {
+        "name":"Ascend",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Ascend.png"
+      },
+      {
+        "name":"Auric1",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Auric1.png"
+      },
+      {
+        "name":"Blade",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Blade.png"
+      },
+      {
+        "name":"Blak",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Blak.png"
+      },
+      {
+        "name":"Blush",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Blush.png"
+      },
+      {
+        "name":"Bora",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Bora.png"
+      },
+      {
+        "name":"Britzer",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Britzer.png"
+      },
+      {
+        "name":"Britzer1",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Britzer1.jpg"
+      },
+      {
+        "name":"CardUi",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/CardUi.png"
+      },
+      {
+        "name":"Cherry",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Cherry.png"
+      },
+      {
+        "name":"Dare",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dare.png"
+      },
+      {
+        "name":"Dare1",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dare1.png"
+      },
+      {
+        "name":"Dazz",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dazz.png"
+      },
+      {
+        "name":"Eco",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Eco.png"
+      },
+      {
+        "name":"Edge",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge.png"
+      },
+      {
+        "name":"Edge1",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge1.png"
+      },
+      {
+        "name":"Edge2",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge2.png"
+      },
+      {
+        "name":"Eminence",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Eminence.png"
+      }
+    ]
+  },
+  {
+    "category_name":"Baloon",
+    "walls":[
+      {
+        "name":"Cherry",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Cherry.png"
+      },
+      {
+        "name":"Dare",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dare.png"
+      },
+      {
+        "name":"Dare1",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dare1.png"
+      },
+      {
+        "name":"Dazz",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Dazz.png"
+      },
+      {
+        "name":"Eco",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Eco.png"
+      },
+      {
+        "name":"Edge",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge.png"
+      },
+      {
+        "name":"Edge1",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge1.png"
+      },
+      {
+        "name":"Edge2",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Edge2.png"
+      },
+      {
+        "name":"Eminence",
+        "link":"https://raw.githubusercontent.com/hiten1985/SamsungThemes/master/Walls/Eminence.png"
+      }
+    ]
+  }
 ]
