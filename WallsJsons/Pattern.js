076a02ea0cbdd254ddd3e517e00b778f970d0c6e
Aditya Gupta <adityaakadynamo@users.noreply.github.com>
2016-09-03 12:30:01 +0530

    Update Pattern.js

--- WallsJsons/Pattern.js
+++ WallsJsons/Pattern.js
@@ -1,39 +1,56 @@
 [
-   {
-		"name": "Ace",
-		"author": "Hiten Soni",
-		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Ace.png"
-	}, {
-		"name": "Arena",
-		"author": "Hiten Soni",
-		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Arena.png"
-	}, {
-		"name": "Arena1",
-		"author": "Hiten Soni",
-		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Arena1.png"
-	}, {
-		"name": "Weyo",
-		"author": "Hiten Soni",
-		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Weyo.png"
-	}, {
-		"name": "Ascend",
-		"author": "Hiten Soni",
-		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Ascend.png"
-	}, {
-		"name": "Eco",
-		"author": "Hiten Soni",
-		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Eco.png"
-	}, {
-		"name": "Rise Ui",
-		"author": "Hiten Soni",
-		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/RiseUi.png"
-	},{
-		"name": "Prime",
-		"author": "Hiten Soni",
-		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Prime.png"
-	}, {
-		"name": "Radiant",
-		"author": "Hiten Soni",
-		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Radiant.png"
-	}
+  {
+    "name": "Rise Ui",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/RiseUi.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/RiseUi.png"
+  }, 
+  {
+    "name": "Ace",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Ace.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Ace.png"
+  }, 
+  {
+    "name": "Arena",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Arena.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Arena.png"
+  }, 
+  {
+    "name": "Arena1",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Arena1.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Arena1.png"
+  }, 
+  {
+    "name": "Weyo",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Weyo.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Weyo.png"
+  }, 
+  {
+    "name": "Ascend",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Ascend.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Ascend.png"
+  }, 
+  {
+    "name": "Eco",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Eco.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Eco.png"
+  }, 
+  {
+    "name": "Prime",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Prime.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Prime.png"
+  }, 
+  {
+    "name": "Radiant",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Radiant.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Radiant.png"
+  }
 ]
