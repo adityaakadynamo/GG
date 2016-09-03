505f28e3b283f22ce57fa91609421c28cc7a6d85
Aditya Gupta <adityaakadynamo@users.noreply.github.com>
2016-09-03 12:31:55 +0530

    Update Material.js

--- WallsJsons/Material.js
+++ WallsJsons/Material.js
@@ -1,59 +1,88 @@
 [
-   {
-   "name": "Vibes",
-   "author": "Hiten Soni",
-   "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Vibes.png"
- }, {
-   "name": "Fusion",
-   "author": "Hiten Soni",
-   "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Fusion.png"
- }, {
-   "name": "Inspire",
-   "author": "Hiten Soni",
-   "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Inspire.png"
- }, {
-   "name": "Material 1",
-   "author": "Hiten Soni",
-   "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Material1.png"
- },
- {
-     "name": "Material 6",
-     "author": "Hiten Soni",
-     "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Material_6.png"
-   },{
+  {
+    "author": "Hiten Soni",
+    "name": "Material Beauty",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Material_8.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Material_8.png"
+    
+  },
+  {
     "author": "Hiten Soni",
     "name": "Soul",
-    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Soul.png"
-
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Soul.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Soul.png"
+    
   },
-               {
-     "name": "Material 7",
-     "author": "Hiten Soni",
-     "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Material_7.png"
-   },
- {
-   "name": "CardUi",
-   "author": "Hiten Soni",
-   "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/CardUi.png"
- }, {
-   "name": "Stripes",
-   "author": "Hiten Soni",
-   "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Stripes.png"
- }, {
-   "name": "Stripes 2",
-   "author": "Hiten Soni",
-   "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Stripes2.png"
- }, {
-   "name": "PerfectNoteUi",
-   "author": "Tim Austin",
-   "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/PerfectNoteUi.png"
- }, {
-   "name": "PerfectUi",
-   "author": "Tim Austin",
-   "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/PerfectUi.png"
- }, {
-   "name": "SupremeUi",
-   "author": "Tim Austin",
-   "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/SupremeUi.png"
- }
+  {
+    "name": "Vibes",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Vibes.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Vibes.png"
+  },
+  {
+    "name": "Fusion",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Fusion.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Fusion.png"
+  }, 
+  {
+    "name": "Inspire",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Inspire.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Inspire.png"
+  }, 
+  {
+    "name": "Material 1",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Material1.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Material1.png"
+  }, 
+  {
+    "name": "Material 6",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Material_6.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Material_6.png"
+  },
+  {
+    "name": "Material 7",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Material_7.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Material_7.png"
+  },
+  {
+    "name": "CardUi",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/CardUi.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/CardUi.png"
+  }, 
+  {
+    "name": "Stripes",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Stripes.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Stripes.png"
+  }, 
+  {
+    "name": "Stripes 2",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Stripes2.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Stripes2.png"
+  }, 
+  {
+    "name": "PerfectNoteUi",
+    "author": "Tim Austin",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/PerfectNoteUi.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/PerfectNoteUi.png"
+  }, 
+  {
+    "name": "PerfectUi",
+    "author": "Tim Austin",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/PerfectUi.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/PerfectUi.png"
+  }, 
+  {
+    "name": "SupremeUi",
+    "author": "Tim Austin",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/SupremeUi.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/SupremeUi.png"
+  } 
 ]
