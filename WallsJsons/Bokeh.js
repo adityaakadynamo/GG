509acf5b0f06ca529d6fb5cf6db446635a3161f2
Aditya Gupta <adityaakadynamo@users.noreply.github.com>
2016-09-03 12:34:33 +0530

    Update Bokeh.js

--- WallsJsons/Bokeh.js
+++ WallsJsons/Bokeh.js
@@ -1,28 +1,38 @@
 [
-   {
-		"name": "Bokeh 1",
-		"author": "Hiten Soni",
-		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Bokeh_1.png"
-	}, {
-		"name": "Bokeh 2",
-		"author": "Hiten Soni",
-		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Bokeh_2.png"
-	}, {
-		"name": "Bokeh 3",
-		"author": "Hiten Soni",
-		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Bokeh_3.png"
-	}, {
-		"name": "Oxygen",
-		"author": "Hiten Soni",
-		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Oxygen.png"
-	},{
-		"name": "Bokeh 4",
-		"author": "Hiten Soni",
-		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Bokeh4.png"
-	},
-	{
-			"name": "Bokeh 5",
-			"author": "Hiten Soni",
-			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Bokeh_5.png"
-		}
+  {
+    "name": "Oxygen",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Oxygen.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Oxygen.png"
+  }, 
+  {
+    "name": "Bokeh 1",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Bokeh_1.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Bokeh_1.png"
+  },
+  {
+    "name": "Bokeh 2",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Bokeh_2.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Bokeh_2.png"
+  }, 
+  {
+    "name": "Bokeh 3",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Bokeh_3.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Bokeh_3.png"
+  }, 
+  {
+    "name": "Bokeh 4",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Bokeh4.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Bokeh4.png"
+  },
+  {
+    "name": "Bokeh 5",
+    "author": "Hiten Soni",
+    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Bokeh_5.png",
+    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Bokeh_5.png"
+  }
 ]
