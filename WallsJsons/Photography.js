a36f4487143f4ab5c53c1a898ab0093781cf373e
Aditya Gupta <adityaakadynamo@users.noreply.github.com>
2016-09-09 09:18:50 +0530

    Update Photography.js

--- WallsJsons/Photography.js
+++ WallsJsons/Photography.js
@@ -1,74 +1,74 @@
-[
-  {
- 		"name": "Indian Mountains",
- 		"author": "Hiten Soni",
- 		"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Mountains-India.jpg",
- 		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Mountains-India.jpg"
- 		},  
-  {
-    "name": "Photography",
-    "author": "Hiten Soni",
-    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_1.jpg",
-    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_1.jpg"
-  },
-  {
-    "name": "Photography 2",
-    "author": "Hiten Soni",
-    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_2.jpg",
-    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_2.jpg"
-  },
-  {
-    "name": "Photography 3",
-    "author": "Hiten Soni",
-    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_3.jpg",
-    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_3.jpg"
-  },
-  {
-    "name": "Photography 4",
-    "author": "Hiten Soni",
-    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_4.jpg",
-    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_4.jpg"
-  },
-  {
-    "name": "Photography 5",
-    "author": "Hiten Soni",
-    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_5.jpg",
-    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_5.jpg"
-  },
-  {
-    "name": "Photography 6",
-    "author": "Hiten Soni",
-    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_6.jpg",
-    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_6.jpg"
-  },
-  {
-    "name": "Photography 7",
-    "author": "Hiten Soni",
-    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_7.jpg",
-    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_7.jpg"
-  },
-  {
-    "name": "Photography 8",
-    "author": "Hiten Soni",
-    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_8.jpg",
-    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_8.jpg"
-  },
-  {
-    "name": "Photography 9",
-    "author": "Hiten Soni",
-    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_9.jpg",
-    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_9.jpg"
-  },
-  {
-    "name": "Photography 10",
-    "author": "Hiten Soni",
-    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_10.jpg",
-    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_10.jpg"
-  },
-  {
-    "name": "Deep",
-    "author": "Hiten Soni",
-    "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Deep.JPG",
-    "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Deep.JPG"
-  }
+[  
+   {  
+      "name":"Indian Mountains",
+      "author":"Hiten Soni",
+      "thumb_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Mountains-India.jpg",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Mountains-India.jpg"
+   },
+   {  
+      "name":"Photography",
+      "author":"Hiten Soni",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_1.jpg",
+      "thumb_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_1.jpg"
+   },
+   {  
+      "name":"Photography 2",
+      "author":"Hiten Soni",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_2.jpg",
+      "thumb_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_2.jpg"
+   },
+   {  
+      "name":"Photography 3",
+      "author":"Hiten Soni",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_3.jpg",
+      "thumb_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_3.jpg"
+   },
+   {  
+      "name":"Photography 4",
+      "author":"Hiten Soni",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_4.jpg",
+      "thumb_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_4.jpg"
+   },
+   {  
+      "name":"Photography 5",
+      "author":"Hiten Soni",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_5.jpg",
+      "thumb_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_5.jpg"
+   },
+   {  
+      "name":"Photography 6",
+      "author":"Hiten Soni",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_6.jpg",
+      "thumb_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_6.jpg"
+   },
+   {  
+      "name":"Photography 7",
+      "author":"Hiten Soni",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_7.jpg",
+      "thumb_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_7.jpg"
+   },
+   {  
+      "name":"Photography 8",
+      "author":"Hiten Soni",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_8.jpg",
+      "thumb_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_8.jpg"
+   },
+   {  
+      "name":"Photography 9",
+      "author":"Hiten Soni",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_9.jpg",
+      "thumb_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_9.jpg"
+   },
+   {  
+      "name":"Photography 10",
+      "author":"Hiten Soni",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_10.jpg",
+      "thumb_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Photography_10.jpg"
+   },
+   {  
+      "name":"Deep",
+      "author":"Hiten Soni",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Deep.JPG",
+      "thumb_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Deep.JPG"
+   }
 ]
