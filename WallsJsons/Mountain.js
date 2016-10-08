86e5499895378cfbe12ac36d593cba54569046ff
Aditya Gupta <adityaakadynamo@users.noreply.github.com>
2016-10-08 09:31:12 +0530

    Update Mountain.js

--- WallsJsons/Mountain.js
+++ WallsJsons/Mountain.js
@@ -1,45 +1,50 @@
-[
-{
-			"author": "Hiten Soni",
-			"name": "Dark Night",
-			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Mountains/Dark_Night.png",
-			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Dark_Night.png"
-
-		},
-              {
-			"name": "Moon",
-			"author": "Hiten Soni",
-			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Mountains/Moon.png",
-			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Moon.png"
-		},
-		{
-			"name": "Mountain",
-			"author": "Hiten Soni",
-			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Mountains/Mountain.png",
-			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Mountain.png"
-		}, 
-		{
-			"name": "Mountain",
-			"author": "Hiten Soni",
-			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Mountains/Mountain_Trials.png",
-			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Mountain_Trials.png"
-		}, 
-		{
-			"name": "Mountain 2",
-			"author": "Hiten Soni",
-			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Mountains/Mountain2.png",
-			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Mountain2.png"
-		}, 
-                {
-			"name": "Mountain 3",
-			"author": "Hiten Soni",
-			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Mountains/Mountain3.png",
-			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Mountain3.png"
-		},
-		{
-			"name": "Sea",
-			"author": "Hiten Soni",
-			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Sea.png",
-			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Sea.png"
-		}
+[  
+   {  
+      "author":"Hiten Soni",
+      "name":"Dark Night",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Mountains/Dark_Night.png",
+      "small_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Dark_Night.png"
+   },
+   {  
+      "author":"Hiten Soni",
+      "name":"Dark Night",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Mountains/Dark_Night.png",
+      "thumb_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Dark_Night.png"
+   },
+   {  
+      "name":"Moon",
+      "author":"Hiten Soni",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Mountains/Moon.png",
+      "thumb_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Moon.png"
+   },
+   {  
+      "name":"Mountain",
+      "author":"Hiten Soni",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Mountains/Mountain.png",
+      "thumb_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Mountain.png"
+   },
+   {  
+      "name":"Mountain",
+      "author":"Hiten Soni",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Mountains/Mountain_Trials.png",
+      "thumb_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Mountain_Trials.png"
+   },
+   {  
+      "name":"Mountain 2",
+      "author":"Hiten Soni",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Mountains/Mountain2.png",
+      "thumb_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Mountain2.png"
+   },
+   {  
+      "name":"Mountain 3",
+      "author":"Hiten Soni",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Mountains/Mountain3.png",
+      "thumb_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Mountain3.png"
+   },
+   {  
+      "name":"Sea",
+      "author":"Hiten Soni",
+      "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Sea.png",
+      "thumb_url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Sea.png"
+   }
 ]
