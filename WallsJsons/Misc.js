5cbe0c2be5e0e582fa30827d67e3c3a7168766b3
Aditya Gupta <adityaakadynamo@gmail.com>
2016-10-31 12:06:18 +0530

    Update

--- WallsJsons/Misc.js
+++ WallsJsons/Misc.js
@@ -1,44 +1,93 @@
 [
+     {
+			"name": "Reactor Ui",
+			"author": "Hiten Soni",
+			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Reactor_Ui.png",
+			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Reactor_Ui.png"
+		}, 
+		{
+			"name": "Automic",
+			"author": "Hiten Soni",
+			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Automic.png",
+			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Automic.png"
+		}, 
+		{
+			"name": "Reactor",
+			"author": "Hiten Soni",
+			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Reactor.png",
+			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Reactor.png"
+		}, 
+		{
+			"name": "Twirl",
+			"author": "Hiten Soni",
+			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Twirl.png",
+			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Twirl.png"
+		}, 
+		{
+			"name": "Riddle",
+			"author": "Hiten Soni",
+			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/RiddleUi.png",
+			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/RiddleUi.png"
+		}, 
                 {
+			"name": "Halloween Night",
+			"author": "Hiten Soni",
+			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Halloween_Night.png",
+			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Halloween_Night.png"
+		}, 
+		{
+			"name": "Dots",
+			"author": "Hiten Soni",
+			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Dots.png",
+			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Dots.png"
+		}, 
+		{
+			"name": "Zig Zag Bricks",
+			"author": "Hiten Soni",
+			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Zigzag_bricks.jpg",
+			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Zigzag_bricks.jpg"
+		}, 
+		{
 			"name": "Cells",
 			"author": "Hiten Soni",
 			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Cells.png",
-			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Cells.png"
+			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Cells.png"
+		}, 
+
+		{
+			"name": "Mkbhd",
+			"author": "Hiten Soni",
+			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Mkbhd.png",
+			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Mkbhd.png"
 		}, 
 		{
 			"name": "Noise 2",
 			"author": "Hiten Soni",
 			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Noise_2.png",
-			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Noise_2.png"
+			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Noise_2.png"
 		}, 
 		{
 			"name": "Noise",
 			"author": "Hiten Soni",
 			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Noise.png",
-			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Noise.png"
+			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Noise.png"
 		}, 
 		{
 			"name": "Random",
 			"author": "Hiten Soni",
 			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/RandomShapes.png",
-			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/RandomShapes.png"
+			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/RandomShapes.png"
 		},  
                 {
 			"name": "Geo",
 			"author": "Hiten Soni",
 			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Geo.png",
-			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Geo.png"
-		}, 
-		{
-			"name": "Mkbhd",
-			"author": "Hiten Soni",
-			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Mkbhd.png",
-			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Mkbhd.png"
+			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Geo.png"
 		}, 
 		{
 			"name": "Ore",
 			"author": "Hiten Soni",
 			"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Ore.png",
-			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Thumbs/Ore.png"
+			"thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Ore.png"
 		}
 ]
