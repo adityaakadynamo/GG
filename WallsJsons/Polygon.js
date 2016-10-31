951dc5de6dda3cdd8968a9ff7276f384d5ce4b9c
Aditya Gupta <adityaakadynamo@gmail.com>
2016-10-31 12:08:28 +0530

    Added

---
+++ WallsJsons/Polygon.js
@@ -0,0 +1,65 @@
+[  
+   
+         "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Polygon4.png"
+      },
+      {
+         "name": "Polygon 5",
+         "author": "Hiten Soni",
+         "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Polygon/Polygon5.png",
+         "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Polygon5.png"
+      },
+      {
+         "name": "Polygon 6",
+         "author": "Hiten Soni",
+         "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Polygon/Polygon6.png",
+         "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Polygon6.png"
+      },
+      {
+         "name": "Polygon 7",
+         "author": "Hiten Soni",
+         "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Polygon/Polygon7.png",
+         "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Polygon7.png"
+      },
+      {
+         "name": "Polygon 8",
+         "author": "Hiten Soni",
+         "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Polygon/Polygon8.png",
+         "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Polygon8.png"
+      },
+      {
+         "name": "Polygon 9",
+         "author": "Hiten Soni",
+         "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Polygon/Polygon9.png",
+         "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Polygon9.png"
+      },
+      {
+         "name": "Polygon 10",
+         "author": "Hiten Soni",
+         "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Polygon/Polygon10.png",
+         "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Polygon10.png"
+      },
+      {
+         "name": "Polygon 11",
+         "author": "Hiten Soni",
+         "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Polygon/Polygon11.png",
+         "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Polygon11.png"
+      },
+      {
+         "name": "Polygon 12",
+         "author": "Hiten Soni",
+         "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Polygon/Polygon12.png",
+         "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Polygon12.png"
+      },
+      {
+         "name": "Polygon 13",
+         "author": "Hiten Soni",
+         "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Polygon/Polygon13.png",
+         "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Polygon13.png"
+      },
+      {
+         "name": "Polygon 14",
+         "author": "Hiten Soni",
+         "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Polygon/Polygon14.png",
+         "thumb_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Polygon14.png"
+      }
+]
