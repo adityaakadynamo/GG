2b96806c322d80d8a129c8592edd2f103f1974c7
Aditya Gupta <adityaakadynamo@users.noreply.github.com>
2016-12-18 18:54:01 +0530

    Update Drawer.js

--- Drawer.js
+++ Drawer.js
@@ -1,38 +1,20 @@
 [  
    {  
-      "category":"Material",
-      "jsonlink":"https://raw.githubusercontent.com/adityaakadynamo/GG/master/WallsJsons/Material.js"
-   },
-   {  
-      "category":"Pattern",
-      "jsonlink":"https://raw.githubusercontent.com/adityaakadynamo/GG/master/WallsJsons/Pattern.js"
-   },
-   {  
-      "category":"Bokeh",
-      "jsonlink":"https://raw.githubusercontent.com/adityaakadynamo/GG/master/WallsJsons/Bokeh.js"
-   },
-   {  
-      "category":"Polygon",
-      "jsonlink":"https://raw.githubusercontent.com/adityaakadynamo/GG/master/WallsJsons/Polygon.js"
-   },
-   {  
-      "category":"Mountain",
-      "jsonlink":"https://raw.githubusercontent.com/adityaakadynamo/GG/master/WallsJsons/Mountain.js"
-   },
-   {  
-      "category":"Misc",
-      "jsonlink":"https://raw.githubusercontent.com/adityaakadynamo/GG/master/WallsJsons/Misc.js"
-   },
-   {  
       "category":"Earth",
-      "jsonlink":"https://raw.githubusercontent.com/adityaakadynamo/GG/master/WallsJsons/Earth.js"
    },
    {  
-      "category":"Outer Space",
-      "jsonlink":"https://raw.githubusercontent.com/adityaakadynamo/GG/master/WallsJsons/Outer%20Space.js"
+      "category":"Water",
    },
    {  
-      "category":"Photography",
-      "jsonlink":"https://raw.githubusercontent.com/adityaakadynamo/GG/master/WallsJsons/Photography.js"
-   }
+      "category":"Glass",
+   },
+   {  
+      "category":"Stuff",
+   },
+   {  
+      "category":"All",
+   },
+   {  
+      "category":"Others",
+   },
 ]
